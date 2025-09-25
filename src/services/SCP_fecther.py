da19a087901e1481c4919604c83237bafe35e978
Angel Amaya <135993385+aamaya33@users.noreply.github.com>
2025-09-25 10:02:40 -0400

    skeleton for scp fetcher

---
+++ src/services/SCP_fecther.py
@@ -0,0 +1,7 @@
+class SCPFetcher:
+    def __init__(self, config):
+        self.config = config
+
+    def fetch_scp(self, source, destination):
+        # Implement SCP fetching logic here
+        pass
